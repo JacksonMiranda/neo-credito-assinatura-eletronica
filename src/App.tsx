8492e709817539be0f56d2e7f4cb5723825905e9
Jackson Jovino Miranda <jackson.j.m@hotmail.com>
2026-05-10 11:06:55 -0300

    chore: configura estrutura base do projeto
    Closes #1, #2

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PainelCorban from './routes/PainelCorban';
+import ValidacaoDossie from './routes/ValidacaoDossie';
+import BarraSuperior from './components/layout/BarraSuperior';
+import styles from './App.module.css';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className={styles.root}>
+        <BarraSuperior />
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<PainelCorban />} />
+            <Route path="/dossie" element={<ValidacaoDossie />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+
